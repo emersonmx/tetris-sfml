653c02b8b88ab13a4a639097b1c491176fdc69e5
Emerson MX <emersonmx@gmail.com>
2016-05-16 21:59:58 -0300

    Simplify Game class

--- src/MXG/Game.cpp
+++ src/MXG/Game.cpp
@@ -4,45 +4,13 @@
 
 namespace mxg {
 
-Game::Game() {
-    states_.emplace(new DefaultState());
-}
-
-State* Game::currentState() {
-    return states_.top().get();
-}
-
-void Game::pushState(State* state) {
-    throwExceptionIfNull(state);
-
-    currentState()->exit();
-    state->enter();
-    states_.emplace(state);
-}
-
-void Game::popState() {
-    if (!statesIsEmpty()) {
-        currentState()->exit();
-        states_.pop();
-        currentState()->enter();
-    }
-}
-
-void Game::clearStates() {
-    while (!statesIsEmpty()) {
-        popState();
-    }
-}
-
 void Game::changeState(State* state) {
     throwExceptionIfNull(state);
 
-    popState();
-    pushState(state);
-}
+    currentState_->exit();
+    state->enter();
 
-bool Game::statesIsEmpty() {
-    return states_.size() <= 1;
+    currentState_ = state;
 }
 
 void Game::throwExceptionIfNull(State* state) {
--- src/MXG/Game.hpp
+++ src/MXG/Game.hpp
@@ -1,9 +1,6 @@
 #ifndef MXG_GAME_HPP_
 #define MXG_GAME_HPP_
 
-#include <memory>
-#include <stack>
-
 #include "MXG/Application.hpp"
 #include "MXG/DefaultState.hpp"
 
@@ -11,19 +8,12 @@ namespace mxg {
 
 class Game : public Application {
     public:
-        Game();
-
-        State* currentState();
-        void pushState(State* state);
-        void popState();
-        void clearStates();
+        State* currentState() { return currentState_; }
         void changeState(State* state);
 
     private:
-        bool statesIsEmpty();
         void throwExceptionIfNull(State* state);
-
-        std::stack< std::unique_ptr<State> > states_;
+        State* currentState_;
 };
 
 } /* namespace mxg */

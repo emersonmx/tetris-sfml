6743267aee816167e7bca5723bf6db2625c5660b
Emerson MX <emersonmx@gmail.com>
2016-09-07 22:54:01 -0300

    Add utils

---
+++ src/Tetris/Utils.cpp
@@ -0,0 +1,20 @@
+#include "Tetris/Utils.hpp"
+
+#include <SFML/Graphics/Rect.hpp>
+
+namespace tetris {
+
+namespace Utils {
+
+sf::Vector2f calculateCenterOfRect(const sf::FloatRect& rect) {
+    return sf::Vector2f(rect.left + rect.width / 2.0f,
+                        rect.top + rect.height / 2.0f);
+}
+
+sf::Vector2f calculateSizeOfRect(const sf::FloatRect& rect) {
+    return sf::Vector2f(rect.left + rect.width, rect.top + rect.height);
+}
+
+} /* namespace Utils */
+
+} /* namespace tetris */

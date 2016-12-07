540243d96b2f42005487c1f54a73f3243ba49f53
Emerson MX <emersonmx@gmail.com>
2016-12-07 16:52:47 -0300

    Update build_assets.sh

--- assets/build_assets.sh
+++ assets/build_assets.sh
@@ -2,4 +2,13 @@
 
 inkscape src/block.svg --export-png=block.png
 inkscape src/grid.svg --export-png=grid.png
+
 lame src/main_music.wav main_music.mp3
+
+normal_files=$(find src/ -iname "*.wav" ! -name "main_music.wav")
+
+for f in $normal_files
+do
+    echo "Copying '$f' to current path"
+    cp -f $f .
+done
